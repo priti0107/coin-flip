8a9d7630da8adf9bc048f49ca95eae4ec4df7026
prevajUser <priti@prevaj.com>
2025-07-19 10:57:38 +0530

    intial commit

---
+++ src/components/Coin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Coin extends Component {
+    render() {
+        return (
+            <div class='Coin'>
+                <img style={{width: '200px', height: '200px'}} src={this.props.info.imgSrc}/>
+            </div>
+        )
+    }
+}
+
+export default Coin
